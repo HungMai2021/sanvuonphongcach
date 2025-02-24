9c294b3681b7c8f06827d418b5875a87cef1c939
HungMai2021 <96099426+HungMai2021@users.noreply.github.com>
2025-02-24 19:37:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     let message = document.getElementById("message").value;
 
     let encodedMessage = encodeURIComponent(`Họ tên: ${name}\nSĐT: ${phone}\nNội dung: ${message}`);
-    let zaloUrl = `https://zalo.me/0828234902?text=${encodedMessage}`;
+    let zaloUrl = `https://zalo.me/0903609889?text=${encodedMessage}`;
 
     window.open(zaloUrl, "_blank");
 });
@@ -18,7 +18,7 @@ document1.getElementById("frmcontact").addEventListener("submit", function(event
     let message1 = document1.getElementById("message").value;
 
     let encodedMessage1 = encodeURIComponent(`Họ tên: ${name1}\nEmail: ${email1}\nNội dung: ${message1}`);
-    let zaloUrl1 = `https://zalo.me/0828234902?text=${encodedMessage1}`;
+    let zaloUrl1 = `https://zalo.me/0903609889?text=${encodedMessage1}`;
 
     window.open(zaloUrl1, "_blank");
 });
